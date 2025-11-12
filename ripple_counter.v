023107d257eff82df4546d1795fbddc076e4a8e1
Oliver Hill <Oliverhill@u.boisestate.edu>
2025-11-12 15:32:36 -0700

    Added ripple counter logic, not finished

--- ripple_counter.v
+++ ripple_counter.v
@@ -1,6 +1,16 @@
-module ripple_counter();
+module ripple_counter(
+    input reset,
+    input clk,
+    output q[2:0]
+);
+
+wire w1, w2, w3;
+
+t_flipflop tff1 (.clk(clk), .reset(reset), .t(1'b1), .q(w1));
+t_flipflop tff2 (.clk(w1), .reset(reset), .t(1'b1), .q(w2));
+t_flipflop tff3 (.clk(w2), .reset(reset), .t(1'b1), .q(w3));
 
 
+//assign w1
 
-
-endmodule;
+endmodule
